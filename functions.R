9249371d8d22429397fe9907d04ad0539dcaf089
NonaR <rothkugelsonja@aol.de>
2015-06-17 13:02:17 +0200

    added plot and raster functions

--- functions.R
+++ functions.R
@@ -51,32 +51,52 @@ get_paleomap <- function (interval){
   shape
 }
 
-getdata_paleomap <- function(base_name, interval, database){
+getdata_paleomap <- function(base_name, interval){
   data <- c()
-  if(database=="paleobiodb"){
+#   if(database=="paleobiodb"){
     occ <- pbdb_occurrences (base_name=base_name, interval=interval, 
-                      show=c("coords", "phylo", "ident", "paleoloc"), 
+                      show=c("paleoloc"), 
                       vocab="pbdb")
     data <- data.frame(occ)
-  }
-  if(database=="gbif"){
-    occ <-occ_search(scientificName = base_name, hasCoordinate=TRUE, 
-    fields = c("family","genus","species","decimalLatitude","decimalLongitude", "earliestEpochOrLowestSeries"), 
-    return = "all")
-    df <- data.frame(occ[3])
-    if(length(df$data.earliestEpochOrLowestSeries)!=0){
-      for(i in 1:length(df$data.earliestEpochOrLowestSeries)){
-        if(!is.na(df$data.earliestEpochOrLowestSeries[i])){
-          if(grep(interval, df$data.earliestEpochOrLowestSeries[i])==1){
-            data <- rbind(data,df[i,])
-          }
-        }
-      }
-    }
-    if(length(df$data.earliestEpochOrLowestSeries)==0 || length(data)==0){
-      print("No occurences found!")
-    }
-  }
+#   }
+#   #not sure
+#   if(database=="gbif"){
+#     occ <-occ_search(scientificName = base_name, hasCoordinate=TRUE, 
+#     fields = c("family","genus","species","decimalLatitude","decimalLongitude", 
+#                "earliestEpochOrLowestSeries"), 
+#                return = "all")
+#     df <- data.frame(occ[3])
+#     if(length(df$data.earliestEpochOrLowestSeries)!=0){
+#       for(i in 1:length(df$data.earliestEpochOrLowestSeries)){
+#         if(!is.na(df$data.earliestEpochOrLowestSeries[i])){
+#           if(grep(interval, df$data.earliestEpochOrLowestSeries[i])==1){
+#             data <- rbind(data,df[i,])
+#           }
+#         }
+#       }
+#     }
+#     if(length(df$data.earliestEpochOrLowestSeries)==0 || length(data)==0){
+#       print("No occurences found!")
+#     }
+#   }
   data
 }
 
+plot_paleomap <- function(base_name, interval){
+  shape <- get_paleomap(interval=interval)
+  data <- getdata_paleomap(base_name=base_name, interval=interval)
+  
+  x11()
+  plot(shape)
+  points (data$paleolng, data$paleolat, pch=19, col="red")
+}
+
+raster_paleomap <- function(shape, data){
+    ras <- raster(shape)
+    r<-rasterize(data[,14:15],ras,fun=sum)
+    x11()
+    plot(r)
+    plot(shape, add=TRUE)
+    points(data$paleolng, data$paleolat, pch=19, col="red")
+    r
+}
