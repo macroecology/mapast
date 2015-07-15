fd9615e0e5d7b81aa43c85dd53a9cb062728dcc9
Sara Varela <svarela@paleobiogeography.org>
2015-07-15 13:47:38 +0200

    added names to the shapefiles

---
+++ R/data_help_docs.R
@@ -0,0 +1,22 @@
+#' Aalenian
+#' 
+#' A shapefile with the location of the continents at that temporal interval. 
+#' 
+#' @source http://www.gplates.org/
+#' @docType data
+#' @keywords datasets
+#' @format shapefile
+#' @name Aalenian
+NULL
+
+#' Aeronian
+#' 
+#' A shapefile with the location of the continents at that temporal interval. 
+#' 
+#' @source http://www.gplates.org/
+#' @docType data
+#' @keywords datasets
+#' @format shapefile
+#' @name Aeronian
+NULL
+
