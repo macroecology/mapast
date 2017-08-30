1d023739661db9dd97119a1366937086a44568a2
NonaR <rothkugelsonja@aol.de>
2017-08-30 12:40:51 +0200

    changed description and added zzz.R for external database

---
+++ R/zzz.R
@@ -0,0 +1,6 @@
+.onLoad <- function(libname, pkgname) {
+  repos = getOption("repos")
+  repos["PaleogeoDB"] = "https://github.com/macroecology/PaleogeoDB"
+  options(repos = repos)
+  invisible(repos)
+}
