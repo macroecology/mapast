4f18c212b1fb495226e11e746cd3f0c6c11d1d96
NonaR <rothkugelsonja@aol.de>
2017-10-18 10:23:55 +0200

    modified zzz.R

--- R/zzz.R
+++ R/zzz.R
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 .onLoad <- function(libname, pkgname) {
-=======
-.onLoad <- function() {
-  repos = getOption("repos")
-  repos["PaleogeoDB"] = "https://github.com/macroecology/PaleogeoDB"
-  options(repos = repos)
-  invisible(repos)
->>>>>>> 050b84fc86fe6902de8ae7beaecbee00b14eaad4
+  # repos = getOption("repos")
+  # repos["PaleogeoDB"] = "https://github.com/macroecology/PaleogeoDB"
+  # options(repos = repos)
+  # invisible(repos)
   
   devtools::install_github("macroecology/paleogeoDB")
 
